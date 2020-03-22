f9cc8b62d5885e951cbdf611c1f612b0c7b7a085
jaruwit suriyo <jaruwit.sur@ascendcorp.com>
2020-03-22 18:21:40 +0700

    [Add] draw diamond increment and decrement for loop solution

--- src/main/java/com/kingbom/logic/DrawStar.java
+++ src/main/java/com/kingbom/logic/DrawStar.java
@@ -2,19 +2,37 @@ package com.kingbom.logic;
 
 public class DrawStar {
 
+    //Start solution for loop increment and decrement
     public void drawDiamondWithNumberForLoop(int number) {
-        for (int i = 0; i <= number; i++) {
-            forLoopDrawWithNumber((number - i), " ");
-            forLoopDrawWithNumber((i * 2) -1 , "*");
+        for (int i = 1; i <= number; i++) {
+            int incrementWhiteSpace =  (number - i);
+            int incrementStar = (i * 2) -1 ;
+            forLoopDrawWithNumber(incrementWhiteSpace, " ");
+            forLoopDrawWithNumber(incrementStar, "*");
             System.out.println();
+        }
+
+        for (int i = 1; i <= number; i++) {
+             forLoopDrawWithNumber(i, " ");
+             int decrementStar = ((number - i) * 2) - 1;
+             revertForLoopDrawWithNumber(decrementStar, "*");
+             System.out.println();
         }
     }
 
+
     public void forLoopDrawWithNumber(int number, String draw) {
-        for (int j = 0; j < number; j++) {
+        for (int j = 1; j <= number; j++) {
             System.out.print(draw);
         }
     }
+
+    public void revertForLoopDrawWithNumber(int number, String draw) {
+        for (int j = number; j > 0; j--) {
+            System.out.print(draw);
+        }
+    }
+    //End solution for loop increment and decrement
 
     public void drawDiamondWithNumberWhileLoop(int number) {
         int i = 0;
@@ -55,4 +73,5 @@ public class DrawStar {
             } while (j < number);
         }
     }
+
 }
